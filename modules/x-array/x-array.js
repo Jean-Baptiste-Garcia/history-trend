be5bbf5235e5b3dae5a5bc47eba7ae429c58d07d
Jean-Baptiste-Garcia <garcia.jean-baptiste@orange.fr>
2015-10-13 06:59:22 +0200

    diffArray code simplification

--- modules/x-array/x-array.js
+++ modules/x-array/x-array.js
@@ -2,9 +2,9 @@
 module.exports = function (config) {
     'use strict';
     var R = require('ramda'),
-        id = config.id,
-        compareId = config.compareId,
-        compareObj = config.compareObj || function (x, y) {return compareId(id(x), id(y)); };
+        id = config.id,                 // function that returns id from obj
+        compareId = config.compareId,   // function that compares id
+        compareObj = config.compareObj || function (x, y) {return compareId(id(x), id(y)); }; // function that compares objects (array sorting)
 
 
     function diffAB(araw, braw) {
@@ -35,25 +35,22 @@ module.exports = function (config) {
         alen = a.length;
         blen = b.length;
 
-        if (alen !== 0 && blen !== 0) {
-            while (true) {
-                aId = id(a[aidx]);
-                bId = id(b[bidx]);
-                cmp = compareId(aId, bId);
-                if (cmp === 0) {
-                    (R.equals(a[aidx], b[bidx]) ? identical : modified).push(aId);
-                    aidx += 1;
+        while (aidx < alen && bidx < blen) {
+            aId = id(a[aidx]);
+            bId = id(b[bidx]);
+            cmp = compareId(aId, bId);
+            if (cmp === 0) {
+                (R.equals(a[aidx], b[bidx]) ? identical : modified).push(aId);
+                aidx += 1;
+                bidx += 1;
+            } else {
+                if (cmp > 0) {
+                    added.push(bId);
                     bidx += 1;
                 } else {
-                    if (cmp > 0) {
-                        added.push(bId);
-                        bidx += 1;
-                    } else {
-                        removed.push(aId);
-                        aidx += 1;
-                    }
+                    removed.push(aId);
+                    aidx += 1;
                 }
-                if (aidx >= alen || bidx >= blen) { break; }
             }
         }
 
